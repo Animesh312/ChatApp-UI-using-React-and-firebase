4746445992bbbacdd52c9202848b93ae4139a978
Animesh312 <88401055+Animesh312@users.noreply.github.com>
2022-11-12 00:22:40 +0530

    Update Sidebar.js

--- Sidebar.js
+++ Sidebar.js
@@ -1,73 +1,37 @@
 import React from 'react'
-import './Chat.css'
+import "./SidebarChat.css"
+import { collection, addDoc } from "firebase/firestore"; 
+import {db} from "./firebase"
+export default function SidebarChat({addNewChat}) {
+  const createChat= async()=>{
+    const group=prompt("Please enter your groupname")
+    if(group){
 
-export default function Chat() {
+try {
+  const docRef = await addDoc(collection(db, "groups"), {
+    name: group,
+  });
+  console.log("Document written with ID: ", docRef.id);
+} catch (e) {
+  console.error("Error adding document: ", e);
+}
+    }
+  }
   return (
-    <div className='chat'>
-      {/*---------------chat header------------- */}
-    <div className='chatHeader'>
-    <img src="https://cdn-icons-png.flaticon.com/128/1177/1177568.png" alt=" "/>
-    <div className='chatHeaderInfo'>
-      <h3>Room name</h3>
-      <p>Last seen....</p>
-    </div>
-    <div className='chatHeaderRight'>
-      <button style={{border:'none'}}>
-      <span className="material-symbols-outlined">
-search
-</span>
-      </button>
-      <button style={{border: "none"}}>
-      <span className="material-symbols-outlined">
-attach_file
-</span>
-      </button>
-      <button style=
-          {{ border: "none" }}>
-          <span className='material-symbols-outlined'>
-            more_vert
-          </span>
-
-
-        </button>
+    !addNewChat?( <div className='sidebarChat'>
+      <img src="https://cdn-icons-png.flaticon.com/128/1177/1177568.png" alt='' />
+    
+    <div className="sidebarChatInfo">
+        <h2> coderoom</h2>
+        <p>This is message</p>
+        
       </div>
-    </div>
-    {/*----------chat body---------- */}
-    <div className='chatBody'>
-      <p className='chatMessage'>
-        <span className='chatName'>Rahul</span>
-        Hello guys
-        <span className='timestamp'>11:11</span>
-      </p>
-      <p className='chatMessage'>
-        <span className='chatName'>Rahul</span>
-        Hello guys
-        <span className='timestamp'>11:11</span>
-      </p>
-      <p className='chatMessage chatReceiver'>
-        <span className='chatName'>Rakesh</span>
-        Hello Guys
-        <span className='timestamp'>12:01</span>
-      </p>
-      
-    </div>
-    {/* ------------chat footer------------- */}
-    <div className='chatFooter'>
-    <span className="material-symbols-outlined">
-mood
-</span>
-      <form>
-        <input type="text" placeholder="Type a message"/>
-        <button type="submit" style={{border:'none'}} ><span className="material-symbols-outlined">
-send
-</span> </button>
-      <button style={{border:'none'}}><span className="material-symbols-outlined">
-mic
-</span></button>
-
-      </form>
-    </div>
-
-    </div>
-  )
+      </div> ):(
+        <div onClick={createChat} className='sidebarChat'>
+          <h3>Add New Chat</h3>
+        </div>
+      )
+     
+      );
+  
 }
